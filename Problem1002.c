20c3b82cfaefa2030a92e53600612384342da812
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 11:53:14 -0300

    Create Problem1002.c

---
+++ Problem1002.c
@@ -0,0 +1,31 @@
+'''A fórmula para calcular a área de uma circunferência é definida como A = π. R 2 . Considerando esse problema que π = 3,14159 :
+Autor: Cristina Ribeiro
+Calcule a área usando a fórmula dada na descrição do problema.
+
+Entrada
+A entrada contém um valor de ponto flutuante (precisão dupla), que é a variável R .
+
+Resultado
+Apresente a mensagem "A =" seguida do valor da variável, como no exemplo abaixo, com quatro casas após o ponto decimal. 
+Use todas as variáveis de precisão dupla. Como todos os problemas, não se esqueça de imprimir o final da linha após o resultado, 
+caso contrário, você receberá "Erro de apresentação".
+'''
+
+
+#include <stdio.h>
+#define n 3.14159
+
+
+
+int main (){
+    double R, A;
+
+
+    scanf("%lf", &R);
+
+    A = n*(R*R);
+
+    printf("A=%.4lf", A);
+    printf("\n");
+return 0;
+}
