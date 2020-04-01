125792b5068c79c48c3cb520e4ea7c1a4586e91c
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 11:54:50 -0300

    Create Problem1003.c

---
+++ Problem1003.c
@@ -0,0 +1,32 @@
+'''Leia dois valores inteiros, neste caso, as variáveis ​​A e B. Depois disso, calcule a soma entre eles e atribua-a à variável SOMA . 
+Escreva o valor desta variável.
+
+Entrada
+Autor:Cristina Ribeiro
+O arquivo de entrada contém 2 números inteiros.
+
+Resultado
+Imprima a variável SOMA com todas as letras maiúsculas, com um espaço em branco antes e depois do sinal de igual seguido pelo valor 
+correspondente à soma de A e B. Como todos os problemas, não esqueça de imprimir o fim da linha, caso contrário você receberá 
+"Erro de apresentação"
+'''
+
+
+#include <stdio.h>
+
+
+int main(){
+
+    int A, B, SOMA;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    SOMA = A+B;
+    printf("SOMA = %d", SOMA);
+    printf("\n");
+
+
+
+return 0;
+}
