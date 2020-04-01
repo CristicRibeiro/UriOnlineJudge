df4cf67741380cfa066e09d514c2f8c6b43e2204
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 11:59:07 -0300

    Create Problem1006.c

---
+++ Problem1006.c
@@ -0,0 +1,33 @@
+'''
+Leia três valores (variáveis ​​A, B e C), que são as três notas do aluno. Em seguida, calcule a média, considerando
+que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. Considere que cada nota pode ir de 0 a 10,0, 
+sempre com uma casa decimal.
+
+Entrada
+Autor: Cristina Ribeiro
+O arquivo de entrada contém 3 valores de pontos flutuantes com um dígito após o ponto decimal.
+
+Resultado
+Imprima MÍDIA (média em português) de acordo com o exemplo a seguir, com um espaço em branco antes e depois do sinal de igual.
+'''
+
+#include <stdio.h>
+
+int main(){
+    float A, B, C, MEDIA;
+    float NotaA = 2, NotaB = 3, NotaC = 5;
+
+    scanf("%f", &A);
+    scanf("%f", &B);
+    scanf("%f", &C);
+
+    MEDIA = ((A*NotaA)+ (B*NotaB) + (C*NotaC))/(NotaA+NotaB+NotaC);
+
+    printf("MEDIA = %.1f", MEDIA);
+    printf("\n");
+
+
+
+return 0;
+
+}
