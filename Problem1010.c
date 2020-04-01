c02b2b131bfb4248cbbd2b1a829df8bb10d1341b
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 20:14:02 -0300

    Update Problem1010.c

--- Problem1010.c
+++ Problem1010.c
@@ -1,7 +1,7 @@
 '''
 Nesse problema, a tarefa é ler o código de um produto 1, o número de unidades do produto 1, o preço de uma unidade do produto 1, 
 o código de um produto 2, o número de unidades do produto 2 e o preço para uma unidade do produto 2. Depois disso, calcule e mostre 
-o valor a ser pago autor:cristina ribeiro
+o valor a ser pago 
 
 Entrada
 O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3 valores: dois inteiros e um valor flutuante com 2 dígitos 
