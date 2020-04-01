97a933a05d6f5c91f99864016bef70c6a5f43f86
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 19:12:13 -0300

    Create Problem1016.c

---
+++ Problem1016.c
@@ -0,0 +1,33 @@
+'''
+Dois carros (X e Y) partem na mesma direção. O carro X sai com uma velocidade constante de 60 km / h e o carro Y sai com uma velocidade 
+constante de 90 km / h.
+
+Em uma hora (60 minutos), o carro Y pode ficar a uma distância de 30 quilômetros do carro X, ou seja, pode ficar um quilômetro a 
+cada 2 minutos.
+autor:cristina ribeiro
+
+Leia a distância (em km) e calcule quanto tempo leva (em minutos) para o carro Y percorrer essa distância em relação ao outro carro.
+
+Entrada
+O arquivo de entrada contém 1 valor inteiro.
+
+Resultado
+Imprima o tempo necessário seguido da mensagem "minutos", que significa minutos em português.
+'''
+
+#include <stdio.h>
+
+int main(){
+    int distancia, tempo, X, Y;
+
+    scanf("%d", &distancia);
+
+    tempo = distancia*2;
+    printf("%d minutos", tempo);
+    printf("\n");
+
+
+
+
+return 0;
+}
