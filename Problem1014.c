6b4f2ae2d58cf79288543dea32f9210e3d29caad
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 18:49:13 -0300

    Create Problem1014.c

---
+++ Problem1014.c
@@ -0,0 +1,30 @@
+'''
+Calcule o consumo médio de um carro, fornecendo a distância total percorrida (em Km) e o total de combustível usado (em litros).
+
+Entrada
+autor:cristina ribeiro
+O arquivo de entrada contém dois valores: um valor inteiro X que representa a distância total (em Km) e o segundo é um número de ponto 
+flutuante Y que  representa o total de combustível usado, com um dígito após o ponto decimal.
+
+Resultado
+Apresente um valor que represente o consumo médio de um carro com 3 dígitos após o ponto decimal, seguido pela mensagem "km / l".
+'''
+
+#include <stdio.h>
+
+int main(){
+    int X;
+    float Y, consumo;
+
+
+    scanf("%d", &X);
+    scanf("%f", &Y);
+
+    consumo = X/Y;
+
+    printf("%.3f km/l", consumo);
+    printf("\n");
+
+
+return 0;
+}
